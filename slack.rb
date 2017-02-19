1fea20cd47a7192115785ec823f65b7d808553d6
Sebastin Kolman <skolman@rocketfuel.com>
2017-02-19 15:08:29 -0800

    slack tst

--- slack.rb
+++ slack.rb
@@ -15,23 +15,46 @@ require 'rspec'
 require 'Date'
 require 'pry-byebug'
 
-$number_regex = /^-?\d*(\d,\d+)*(\.\d+(e\d+)?)?$/
-$alpha_regex = /^[A-Za-z]+$/
-$alpha_prefix_regex = /^([A-Za-z]+)(.+)$/
-$numeric_prefix_regex = /^([0-9]+)(.*)$/
+#TODO: Leave comments about cases not covered
+
+#TODO: Leave comments about Using globals , though not advisible
+$letters_spaces = /[A-Za-z\s]+/
+$valid_number_format = /-?\d*(\d,\d+)*(\.\d+(e\d+)?)?/
+$number_regex = /^#{$valid_number_format}$/
+$alpha_regex = /^#{$letters_spaces}$/
+$alpha_prefix_regex = /^(#{$letters_spaces})(.+)$/
+$numeric_prefix_regex = /^(#{$valid_number_format})(.*)$/
+#TODO Better Regex matcher
 $date_regex = /^(\d{4}[\\\/-]{1}\d{1,2}[\\\/-]{1}\d{1,2})$|^(\d{1,2}[\\\/-]{1}\d{1,2}[\\\/-]{1}\d{4})$/
+
+$date_sorter = Proc.new do |d1, d2|
+  Date.parse(d1) <=> Date.parse(d2)
+end
+
+$alpha_sorter = Proc.new do |a1, a2|
+  a1.downcase <=> a2.downcase
+end
+
+$number_sorter = Proc.new do |n1, n2|
+  n1.tr(',','').to_f <=> n2.tr(',','').to_f #if it is a number like "20,000", I remove the comma so that ruby can convert it to the correct float value
+end
+
+def map_prefix_to_suffixes prefix_map, matches
+  if prefix_map[matches[1]].nil?
+    prefix_map[matches[1]] = [matches[-1]]
+  else
+    prefix_map[matches[1]].push matches[-1]
+  end
+end
 
 def sort_string input
 
-
   return input if input.size == 0 || input.size == 1
 
-  dates = []
-  alphas = []
-  numerics = []
-  alpha_prefixes = {}
-  numeric_prefixes = {}
-  others = []
+  dates, alphas, numerics, others = [], [], [], []
+  alpha_prefixes, numeric_prefixes = {}, {}
+
+
 
   input.each do |str|
     if !str.match($date_regex).nil?
@@ -43,40 +66,34 @@ def sort_string input
     elsif !str.match($number_regex).nil?
       numerics.push str
       next
-    elsif !(alpha_numeric = str.match($alpha_prefix_regex)).nil?
-      if alpha_prefixes[alpha_numeric[1]].nil?
-        alpha_prefixes[alpha_numeric[1]] = [alpha_numeric[2]]
-      else
-        alpha_prefixes[alpha_numeric[1]].push alpha_numeric[2]
-      end
-    elsif !(numeric_prefix = str.match($numeric_prefix_regex)).nil?
-      if mixed[numeric_prefix[1]].nil?
-        mixed[numeric_prefix[1]] = [numeric_prefix[2]]
-      else
-        mixed[numeric_prefix[1]].push numeric_prefix[2]
-      end
+    elsif !(matches = str.match($alpha_prefix_regex)).nil?
+      map_prefix_to_suffixes alpha_prefixes, matches
+    elsif !(matches = str.match($numeric_prefix_regex)).nil?
+      map_prefix_to_suffixes numeric_prefixes, matches
     else
       others.push str
     end
   end
 
   #using custom sorters for maintaining the original string format
-  dates.sort! { |d1, d2| Date.parse(d1) <=> Date.parse(d2)}
-  alphas.sort! { |a1, a2| a1.downcase <=> a2.downcase}
-  numerics.sort! {|n1, n2| n1.tr(',','').to_f <=> n2.tr(',','').to_f} #if it is a number like "20,000", I remove the comma so that ruby can convert it to the correct float value
+  dates.sort! &$date_sorter
+  alphas.sort! &$alpha_sorter
+  numerics.sort! &$number_sorter
 
   #For strings with multiple types, I assume the string either starts with a alpha characters or numbers.
   #I break down the string as a prefix and suffix and group all the strings with same prefix using a hash, with the prefix as key and the array of suffixes as the value.
   #I can then call sort_string recursively on the values of the hash.
   #This obviously does not handle string that start with special characters. For simplicity and for the time limit of this assignment, I ignore that case but we can handle them like
   #any other case mentioned here.
-  alpha_prefixes_keys = alpha_prefixes.keys.sort { |a1, a2| a1.downcase <=> a2.downcase}
+  #
+  #TODO: see if you can sort the hash in palce
+  alpha_prefixes_keys = alpha_prefixes.keys.sort &$alpha_sorter
   alpha_prefixes.keys.each {|key| alpha_prefixes[key] =  sort_string(alpha_prefixes[key])}
 
-  numeric_prefixes_keys = numeric_prefixes.keys.sort {|n1, n2| n1.tr(',','').to_f <=> n2.tr(',','').to_f}
-  numeric_prefixes.keys.each {|key| numeric_prefixes[key] =  sort_string(alpha_prefixes[key])}
+  numeric_prefixes_keys = numeric_prefixes.keys.sort &$number_sorter
+  numeric_prefixes.keys.each {|key| numeric_prefixes[key] =  sort_string(numeric_prefixes[key])}
 
-
+  #TODO: see if you can make this cleaner, using map or reduce perhaps
   sorted_alpha_numerics = []
   alpha_prefixes_keys.each do |key|
     alpha_prefixes[key].each do |str|
@@ -142,7 +159,7 @@ describe '#sort_string' do
     expect(sort_string input).to eq ["2016-10-10", "2016-10-12", "2017-01-01"]
   end
 
-  it 'sorts mutli-format strings' do
+  it 'sorts an array of strings of different types' do
     input = ["1", "banana" , "8", "3", "apple"]
     expect(sort_string input).to eq  ["apple", "banana",  "1", "3", "8"]
   end
@@ -154,6 +171,11 @@ describe '#sort_string' do
     expect(sort_string input).to eq ["one"]
     input = [""]
     expect(sort_string input).to eq [""]
+  end
+
+  it 'sorts strings with multuple formats' do
+    input = ["OS X 10 beta: Kodiak", "OS X 10.11: El Capitan", "OS X 10.4.4 Tiger", "OS X 10.8 Mountain Lion", "OS X 10.6 Snow Leopard"]
+    expect(sort_string input).to eq ["OS X 10 beta: Kodiak", "OS X 10.11: El Capitan", "OS X 10.4.4 Tiger", "OS X 10.6 Snow Leopard", "OS X 10.8 Mountain Lion"]
   end
 
 end
