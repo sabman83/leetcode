c6bc391d71109a7c8af0d48de501b45ba6cea3d9
Sebastin Kolman <skolman@rocketfuel.com>
2017-02-20 12:36:18 -0800

    slack stuff

--- slack.rb
+++ slack.rb
@@ -1,30 +1,24 @@
-#Write a function that takes in a list of strings, and returns the same list sorted in a human-friendly way.
-#Some different cases you should handle include the following:
-#● Numbers: ["-1", "2", ".2", "10", "-2.4"] => ["-2.4", "-1", ".2", "2", "10"]
-#● Dates: ["2016-10-12", "2016-10-10", "2017-01-01"] => ["2016-10-10", "2016-10-12",
-#"2017-01-01"]
-#● Alphabetic strings (ignore case): ["Apple", "Watermelon", "bacon"] => ["Apple", "bacon",
-#"Watermelon"]
-#If a string is a concatenation of multiple types, resolve the types in the order they appear:
-# for example ["android2.2", "Android13.0", "iOS1.0", "iOS1.3"] would be in acceptable sorted order.
-# The list might also have more than one acceptable output (both ["1", "3", "apple"] and ["apple", "1", "3"]
-# are reasonable).
-#If there is a case not explicitly specified above, use your best judgment.
-
 require 'rspec'
 require 'Date'
-require 'pry-byebug'
 
 #TODO: Leave comments about cases not covered
+  #For strings with multiple types, I assume the string either starts with a alpha characters or numbers.
+  #I break down the string as a prefix and suffix and group all the strings with same prefix using a hash, with the prefix as key and the array of suffixes as the value.
+  #I can then call sort_string recursively on the values of the hash.
+  #This obviously does not handle string that start with special characters. For simplicity and for the time limit of this assignment, I ignore that case but we can handle them like
+  #any other case mentioned here.
+  #
 
-#TODO: Leave comments about Using globals , though not advisible
-$letters_spaces = /[A-Za-z\s]+/
+#Though using global variables is not advisable, using these for the sake of this assignment.
+#Ideally, I would define these in a class.
 $valid_number_format = /-?\d*(\d,\d+)*(\.\d+(e\d+)?)?/
 $number_regex = /^#{$valid_number_format}$/
+$numeric_prefix_regex = /^(#{$valid_number_format})(.*)$/
+
+$letters_spaces = /[A-Za-z\s]+/
 $alpha_regex = /^#{$letters_spaces}$/
 $alpha_prefix_regex = /^(#{$letters_spaces})(.+)$/
-$numeric_prefix_regex = /^(#{$valid_number_format})(.*)$/
-#TODO Better Regex matcher
+
 $date_regex = /^(\d{4}[\\\/-]{1}\d{1,2}[\\\/-]{1}\d{1,2})$|^(\d{1,2}[\\\/-]{1}\d{1,2}[\\\/-]{1}\d{4})$/
 
 $date_sorter = Proc.new do |d1, d2|
@@ -39,14 +33,6 @@ $number_sorter = Proc.new do |n1, n2|
   n1.tr(',','').to_f <=> n2.tr(',','').to_f #if it is a number like "20,000", I remove the comma so that ruby can convert it to the correct float value
 end
 
-def map_prefix_to_suffixes prefix_map, matches
-  if prefix_map[matches[1]].nil?
-    prefix_map[matches[1]] = [matches[-1]]
-  else
-    prefix_map[matches[1]].push matches[-1]
-  end
-end
-
 def sort_string input
 
   return input if input.size == 0 || input.size == 1
@@ -54,22 +40,18 @@ def sort_string input
   dates, alphas, numerics, others = [], [], [], []
   alpha_prefixes, numeric_prefixes = {}, {}
 
-
-
   input.each do |str|
-    if !str.match($date_regex).nil?
+    case
+    when valid_date?(str)
       dates.push str
-      next
-    elsif !str.match($alpha_regex).nil?
+    when str.match($alpha_regex)
       alphas.push str
-      next
-    elsif !str.match($number_regex).nil?
+    when str.match($number_regex)
       numerics.push str
-      next
-    elsif !(matches = str.match($alpha_prefix_regex)).nil?
-      map_prefix_to_suffixes alpha_prefixes, matches
-    elsif !(matches = str.match($numeric_prefix_regex)).nil?
-      map_prefix_to_suffixes numeric_prefixes, matches
+    when (matches = str.match($alpha_prefix_regex))
+      map_prefix_to_suffixes(alpha_prefixes, matches)
+    when (matches = str.match($numeric_prefix_regex))
+      map_prefix_to_suffixes(numeric_prefixes, matches)
     else
       others.push str
     end
@@ -80,68 +62,53 @@ def sort_string input
   alphas.sort! &$alpha_sorter
   numerics.sort! &$number_sorter
 
-  #For strings with multiple types, I assume the string either starts with a alpha characters or numbers.
-  #I break down the string as a prefix and suffix and group all the strings with same prefix using a hash, with the prefix as key and the array of suffixes as the value.
-  #I can then call sort_string recursively on the values of the hash.
-  #This obviously does not handle string that start with special characters. For simplicity and for the time limit of this assignment, I ignore that case but we can handle them like
-  #any other case mentioned here.
-  #
-  #TODO: see if you can sort the hash in palce
-  alpha_prefixes_keys = alpha_prefixes.keys.sort &$alpha_sorter
-  alpha_prefixes.keys.each {|key| alpha_prefixes[key] =  sort_string(alpha_prefixes[key])}
-
-  numeric_prefixes_keys = numeric_prefixes.keys.sort &$number_sorter
-  numeric_prefixes.keys.each {|key| numeric_prefixes[key] =  sort_string(numeric_prefixes[key])}
-
-  #TODO: see if you can make this cleaner, using map or reduce perhaps
-  sorted_alpha_numerics = []
-  alpha_prefixes_keys.each do |key|
-    alpha_prefixes[key].each do |str|
-      sorted_alpha_numerics.push (key + str)
-    end
-  end
-
-  sorted_numerics_alpha = []
-  numeric_prefixes_keys.each do |key|
-    numeric_prefixes[key].each do |str|
-      sorted_numerics_alpha.push (key + str)
-    end
-  end
+  sorted_alpha_numerics = sort_prefix_concat_suffix( alpha_prefixes, $alpha_sorter )
+  sorted_numerics_alpha = sort_prefix_concat_suffix( numeric_prefixes, $number_sorter )
 
   others.sort!
 
   return dates + alphas + sorted_alpha_numerics + numerics + sorted_numerics_alpha + others
 end
 
+#Utility Methods
+#TODO Comments/Documentation
+def map_prefix_to_suffixes prefix_map, matches
+  if prefix_map[matches[1]].nil?
+    prefix_map[matches[1]] = [matches[-1]]
+  else
+    prefix_map[matches[1]].push matches[-1]
+  end
+end
 
-  #parse each item in array and put in their respective arrays
-  #sort each array and return in some order
+def sort_prefix_concat_suffix hash_map, sorter
+  keys = hash_map.keys.sort &sorter
+  result = []
+  keys.each do |key|
+    hash_map[key] =  sort_string(hash_map[key]) if hash_map[key].size > 1
+    hash_map[key].each do |str|
+      result.push (key + str)
+    end
+  end
+  result
+end
 
-  #remove commas from numbers
-  #caseinsensitive for alpha
+def valid_date? str
+  begin
+    result = str.match($date_regex) && Date.parse(str)
+  rescue ArgumentError => e
+    return false
+  else
+    return result
+  end
+end
+
   #date parsing is not perfect, mention edge cases
-  #else it is mixed types
-  #split it at the type and maybe recursively call sort_string?
-  #for mixed types jave a prefix to suffix hash, sort prefix, then sort suffix for each recursively
-  #may have to add sort function for each type to avoid having to change the string format.
-  #will miss many edge cases, and intelligent string parsing
-  #alpha prefix and numeric prefix should be same as alpha number regexes
 
-input = ["-1", "2", ".2", "10", "-2.4"]
-sort_string input
-
-#parse all types, put then in separate arrays
-#split multi-types and put them in separate arrays
-
-
-
+#TODO: Add more test cases
 describe '#sort_string' do
-#^-?[\d]+(\,\d+)*(\.\d*)?$
-  #^-?\d+(,\d+)*(\.\d+(e\d+)?)?$
-#remove commas
   it 'sorts numbers' do
-    input = ["-1", "2", ".2", "10", "-2.4"]
-    expect(sort_string input).to eq ["-2.4", "-1", ".2", "2", "10"]
+    input = ["-1", "2", ".2", "10", "-2.4", ".2e1"]
+    expect(sort_string input).to eq ["-2.4", "-1", ".2", "2", ".2e1", "10"]
   end
 
   it 'sorts alpha strings' do
@@ -154,9 +121,9 @@ describe '#sort_string' do
      expect(sort_string input).to eq ["android2.2", "Android13.0", "iOS1.0", "iOS1.3"]
   end
 
-  it 'sorts dates' do
-    input = ["2016-10-12", "2017-01-01", "2016-10-10" ]
-    expect(sort_string input).to eq ["2016-10-10", "2016-10-12", "2017-01-01"]
+  it 'sorts dates' do #2013-02-29 is an invalid date and hence will be parsed as non-date string and appended at the end
+    input = ["2016-10-12", "2013-02-29", "2017-01-01", "2016-10-10" ]
+    expect(sort_string input).to eq ["2016-10-10", "2016-10-12", "2017-01-01", "2013-02-29"]
   end
 
   it 'sorts an array of strings of different types' do
@@ -176,9 +143,32 @@ describe '#sort_string' do
   it 'sorts strings with multuple formats' do
     input = ["OS X 10 beta: Kodiak", "OS X 10.11: El Capitan", "OS X 10.4.4 Tiger", "OS X 10.8 Mountain Lion", "OS X 10.6 Snow Leopard"]
     expect(sort_string input).to eq ["OS X 10 beta: Kodiak", "OS X 10.11: El Capitan", "OS X 10.4.4 Tiger", "OS X 10.6 Snow Leopard", "OS X 10.8 Mountain Lion"]
+
+    input = ["a1a1a1a1a1a1","2b2b2b2b2b2b2","c3c3c3c3c3c3c3"]
+    expect(sort_string input).to eq ["a1a1a1a1a1a1", "c3c3c3c3c3c3c3", "2b2b2b2b2b2b2"]
   end
 
 end
+
+describe '#valid_date?' do
+  it 'validates the dates in format yyyy-mm-dd or dd/mm/yyyy' do
+    expect(valid_date? "2017-10-02").to be_truthy
+    expect(valid_date? "2017/10/02").to be_truthy
+    expect(valid_date? "02-10-2017").to be_truthy
+    expect(valid_date? "02/10/2017").to be_truthy
+  end
+
+  it 'does not validate strings in format mm-yyyy-dd' do
+    expect(valid_date? "02-2016-20").to be_falsey
+  end
+
+  it 'does not validate invalid dates' do
+    expect(valid_date? "2013-02-29"). to be_falsey
+    expect(valid_date? "2016-22-29"). to be_falsey
+    expect(valid_date? "2016-03-32"). to be_falsey
+  end
+end
+
 
 
 
